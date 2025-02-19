179a61405def39e8233d2378cc1b676cf31da36f
Jonathan Stronkhorst <JStronkhorst@svb.nl>
2025-02-19 11:16:35 +0100

    first commit

---
+++ src/main/java/dev/jonathanstronkhorst/modularpizzahut/ModularPizzaHutApplication.java
@@ -0,0 +1,13 @@
+package dev.jonathanstronkhorst.modularpizzahut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModularPizzaHutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModularPizzaHutApplication.class, args);
+    }
+
+}
