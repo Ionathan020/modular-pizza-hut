f3499b8f799c4d5b799fe58557a28949054ecf5e
Jonathan Stronkhorst <JStronkhorst@svb.nl>
2025-02-27 13:46:27 +0100

    Testing the modules

--- src/test/java/dev/jonathanstronkhorst/modularpizzahut/ModularPizzaHutApplicationTests.java
+++ src/test/java/dev/jonathanstronkhorst/modularpizzahut/ModularPizzaHutApplicationTests.java
@@ -2,12 +2,34 @@ package dev.jonathanstronkhorst.modularpizzahut;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
 
 @SpringBootTest
 class ModularPizzaHutApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void createApplicationModuleModel() {
+        ApplicationModules modules = ApplicationModules.of(ModularPizzaHutApplication.class);
+        modules.forEach(System.out::println);
+    }
+
+    @Test
+    void verifiesModularStructure() {
+        ApplicationModules modules = ApplicationModules.of(ModularPizzaHutApplication.class);
+        modules.verify();
+    }
+
+    @Test
+    void createModuleDocumentation() {
+        ApplicationModules modules = ApplicationModules.of(ModularPizzaHutApplication.class);
+        new Documenter(modules)
+                .writeDocumentation()
+                .writeIndividualModulesAsPlantUml();
     }
 
 }
