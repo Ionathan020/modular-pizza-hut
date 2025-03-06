b0406b191a416c2b7500d474bd7764db6efc2cec
Jonathan Stronkhorst <JStronkhorst@svb.nl>
2025-03-06 13:12:06 +0100

    Removing classes that need to be created by the attendees

--- src/main/java/dev/jonathanstronkhorst/modularpizzahut/deliverydriver/infrastructure/DeliveryOfPizzaJPARepository.java
+++
@@ -1,9 +0,0 @@
-package dev.jonathanstronkhorst.modularpizzahut.deliverydriver.infrastructure;
-
-import java.util.Optional;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeliveryOfPizzaJPARepository  extends JpaRepository<DeliveryOfPizzaDocument, Long> {
-    Optional<DeliveryOfPizzaDocument> findByOrderReference(UUID orderReference);
-}
