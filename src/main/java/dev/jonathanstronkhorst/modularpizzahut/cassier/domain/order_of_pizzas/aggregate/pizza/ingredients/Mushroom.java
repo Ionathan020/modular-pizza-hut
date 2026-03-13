49c544980cbcb75866c009bbfa9bbcc003d02e65
Jonathan Stronkhorst <jonathan@codefoundry.nl>
2026-03-13 11:46:52 +0100

    Added `Mushroom` and `TomatoSauce` ingredients, and introduced `Margherita` and `Veggie` pizzas to the menu configuration.

---
+++ src/main/java/dev/jonathanstronkhorst/modularpizzahut/cassier/domain/order_of_pizzas/aggregate/pizza/ingredients/Mushroom.java
@@ -0,0 +1,7 @@
+package dev.jonathanstronkhorst.modularpizzahut.cassier.domain.order_of_pizzas.aggregate.pizza.ingredients;
+
+public class Mushroom extends Ingredient {
+    public Mushroom(int quantity) {
+        super(quantity);
+    }
+}
